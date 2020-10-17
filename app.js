6ad5fa9d66cdf8a8082975c112a424635de55c17
Vikas Soni <vikas@Vikass-MacBook-Pro.local>
2020-10-17 20:41:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("hello from nodejs");
+});
+
+app.get('/api/users', (req, res) => {
+    res.json({
+        'name': 'Vikas',
+        'city': 'Noida'
+    });
+});
+
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log("listening to port", port);
+})
